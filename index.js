7a09d5e98f9e0df2d8edb6552b095a95df28daee
UIT24730058 <24730058@ms.uit.edu.vn>
2025-06-29 17:40:54 +0700

    thay doi trang thai

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 require('dotenv').config();
-
+const bodyParser = require('body-parser');
 
 // Import and connect to the database
 const database = require('./config/database');
@@ -9,6 +9,9 @@ database.connect();
 // Initialize the Express application
 const app = express();
 const port = process.env.PORT || 3000;
+
+// parse application/json
+app.use(bodyParser.json());
 
 const routeClient = require("./api/v1/routes/client/index.route");
 routeClient(app);
